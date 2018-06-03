a64173ef0a8cc1f9c4a70cf0361b30d5a7df12ae
nkrivous <kolsln@list.ru>
2018-06-03 18:09:12 +0300

    Add babel

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = { 
     entry: './index.js',
-    module: { },
+    module: { rules: [
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: 'babel-loader'
+        }
+    ]},
    plugins: [new HtmlWebpackPlugin()]
 }
